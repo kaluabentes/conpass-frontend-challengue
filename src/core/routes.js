990d9983608470978a8b4d85b4fae7c38cba5f05
Mac <mac@MacBook-Pro-de-Mac.local>
2019-11-10 21:10:30 -0300

    Setup basic structure

---
+++ src/core/routes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+  Switch,
+  Route
+} from 'react-router-dom'
+
+import Index from '../pages/index'
+
+const Routes = () => (
+  <Switch>
+    <Route path="/" component={Index} />
+  </Switch>
+)
+
+export default Routes
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Index = () => <h1>Index page</h1>
+
+export default Index
