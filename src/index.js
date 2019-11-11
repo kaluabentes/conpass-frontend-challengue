b6a6e5dca65e2fc42176698487be3b13813760cc
Kaluã Bentes <kaluanbentes@gmail.com>
2019-11-11 19:51:19 -0300

    Config base styles

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import "./styles/base.css"
-import App from './core/app'
-import * as serviceWorker from './utils/service-worker'
+import "styles/base.css";
+import App from "core/app";
+import * as serviceWorker from "utils/service-worker";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
