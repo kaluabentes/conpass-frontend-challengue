8fb8cb0425895f9665edd4fde449d8c3c929ce6b
Kaluã Bentes <kaluanbentes@gmail.com>
2019-11-11 21:58:13 -0300

    Add page template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-const Index = () => <h1>Index page</h1>
+import Page from 'components/templates/page'
+import Metadata from 'components/atoms/metadata'
+
+const Index = () => (
+  <Page>
+    <Metadata
+      title="Conpass Hotspots"
+      description="Create hotspots to teach your users about the app"
+    />
+    <h1>Index page</h1>
+  </Page>
+)
 
 export default Index
