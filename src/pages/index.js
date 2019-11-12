8b0e2f0b8d2bf0efcbf24737cede5091dd793622
Kaluã Bentes <kaluanbentes@gmail.com>
2019-11-12 18:42:47 -0300

    Add hotspot components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 import Page from 'components/templates/page'
 import Metadata from 'components/atoms/metadata'
+import Button from 'components/atoms/button'
+import HotspotList from 'components/molecules/hotspot-list'
+import HotspotItem from 'components/atoms/hotspot-item'
+import Container from 'components/atoms/container'
+
+import styles from './styles.module.css'
 
 const Index = () => (
   <Page>
@@ -9,7 +15,18 @@ const Index = () => (
       title="Conpass Hotspots"
       description="Create hotspots to teach your users about the app"
     />
-    <h1>Index page</h1>
+    <Container>
+      <div className={styles.hotspots}>
+        <div className={styles.hotspotsHeader}>
+          <Button>Create Hotspot</Button>
+        </div>
+        <HotspotList title="List of hotspots">
+          <HotspotItem>Hotspot #1</HotspotItem>
+          <HotspotItem>Hotspot #2</HotspotItem>
+          <HotspotItem>Hotspot #3</HotspotItem>
+        </HotspotList>
+      </div>
+    </Container>
   </Page>
 )
 
