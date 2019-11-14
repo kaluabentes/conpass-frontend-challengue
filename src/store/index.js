f89d054f36fa4d78d3b35987a22415c484333433
Kaluã Bentes <kaluanbentes@gmail.com>
2019-11-14 02:06:47 -0300

    Persist state in localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,21 @@
 import { createStore } from 'redux'
+import throttle from 'lodash/throttle'
 
 import rootReducer from './modules/rootReducer'
+import { loadState, saveState } from 'utils/local-storage'
+
+const persistedState = loadState()
 
 const store = createStore(
   rootReducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+store.subscribe(
+  throttle(() => {
+    saveState(store.getState())
+  }, 1000)
 )
 
 export default store
